7b84846a1b98bd982abf5ae3dc4057a81876caa5
Mattwong3 <47612621+Mattwong3@users.noreply.github.com>
2019-09-26 11:21:31 -0700

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,26 @@
+public class InsertionSort implements SortingAlgorithm {
+
+    public InsertionSort(){
+
+
+
+    }
+
+    @Override
+    public void sort(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; ++i) {
+            int key = a[i];
+            int j = i - 1;
+
+
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = key;
+        }
+
+
+    }
+}
